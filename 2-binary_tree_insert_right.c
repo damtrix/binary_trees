4abe9d93ca32de717ad2f493a035a4e1c638352b
damtrix <oludamola.onarinde@gmail.com>
2023-03-02 02:17:19 -0800

    2-binary_tree_insert_right.c

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -20,12 +20,12 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 
-	new = (binary_tree_t *)malloc(sizeof(binary_tree_t));
-
 	if (!parent)
 	{
 		return (NULL);
 	}
+
+	new = malloc(sizeof(binary_tree_t));
 
 	if (!new)
 	{
