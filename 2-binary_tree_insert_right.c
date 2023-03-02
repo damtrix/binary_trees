c51d92fd62c2d12b739c515b59aa9f9f39d92cac
damtrix <oludamola.onarinde@gmail.com>
2023-03-02 01:51:43 -0800

    2-binary_tree_insert_right.c

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -38,7 +38,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	new->right = parent->right;
 	parent->right = new;
 
-	if (new-right)
+	if (new->right)
 	{
 		new->right->parent = new;
 	}
